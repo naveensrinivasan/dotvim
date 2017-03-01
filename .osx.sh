7e2af1926604ade453e53a5921521b22e73f1d00
naveen <nsrinivasan1976@gmail.com>
2017-02-28 20:37:46 -0500

    Updated the brew command

---
+++ .osx.sh
@@ -0,0 +1,21 @@
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
+
+# Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+#git settings
+git config --global user.name "naveen"
+
+git config --global user.email "nsrinivasan1976@gmail.com"
+
+git config --global credential.helper osxkeychain

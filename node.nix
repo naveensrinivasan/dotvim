52a931b8a9f0944dae22d855fb092e162202c6b6
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-10-10 21:37:29 +0000

    Some tweaks

---
+++ node.nix
@@ -0,0 +1,34 @@
+# shell.nix
+with (import <nixpkgs> {});
+mkShell {
+  buildInputs = [
+	 zsh
+	 curl
+	 glow
+	 jq
+	 neovim
+	 gh
+	 oh-my-zsh
+	 coreutils
+	 fzf
+	 ripgrep
+	 powerline-fonts
+	 docker
+	 nodejs
+	 docker-machine
+	 tree
+	 graphviz
+	 fd
+	 delta
+	 bat
+	 shellcheck
+     openssl
+	 zplug
+	 yarn
+	 nodejs
+	 podman
+	 doctl
+    go
+	ag
+  ];
+}

123f41c319c7a1bf8fa1ab29be086d54aa05761f
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-12-19 19:00:34 +0000

    Some tweaks

---
+++ lightning.nix
@@ -0,0 +1,42 @@
+# shell.nix
+with (import <nixpkgs> {});
+mkShell {
+  buildInputs = [
+	 zsh
+	 curl
+	 glow
+	 jq
+	 neovim
+	 gh
+	 oh-my-zsh
+	 coreutils
+	 fzf
+	 ripgrep
+	 powerline-fonts
+	 docker
+	 nodejs
+	 docker-machine
+	 tree
+	 graphviz
+	 fd
+	 delta
+	 bat
+	 shellcheck
+     openssl
+	 zplug
+	 yarn
+	 nodejs
+	 podman
+	 doctl
+    go
+	ag
+        hadolint
+	pkgconfig
+	yubikey-agent
+	httpie
+	 kubectl
+	 kubectx
+         python
+         clang
+  ];
+}

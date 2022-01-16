c17de68494bc51e7c6342856d204398d28076b2f
naveen <172697+naveensrinivasan@users.noreply.github.com>
2022-01-16 18:39:09 +0000

    Some tweaks

---
+++ rust.nix
@@ -0,0 +1,45 @@
+# shell.nix
+with (import <nixpkgs> {});
+mkShell {
+  buildInputs = [
+	 zsh
+	 curl
+     glow
+	 jq
+ 	 neovim
+	 kubectl
+	 gh
+	 oh-my-zsh
+	 coreutils
+	 fzf
+	 ripgrep
+	 powerline-fonts
+	 kubectx
+	 docker
+	 nodejs
+	 docker-machine
+	 tree
+	 graphviz
+	 fd
+	 delta
+	 bat
+	 shellcheck
+	 google-cloud-sdk
+     openssl
+     zplug
+     yarn
+     nodejs
+     podman
+     doctl
+     hugo
+     whois
+     go_1_17
+     protoc-gen-go
+     python
+     clang
+     kind
+     automake
+     rustc
+     rustfmt
+  ];
+}
